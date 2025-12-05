7484da18bc541bd70f9d44915b4f4223a3c6e43a
colin <163393924+officialcc@users.noreply.github.com>
2025-12-05 17:01:20 +0800

    Production main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
+	// "github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -26,9 +26,12 @@ var collection *mongo.Collection
 func main() {
 	fmt.Println("Hello World")
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file:", err)
+	if os.Getenv("ENV") != "production" {
+		// To load local .env if not in production
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file:", err)
+		}
 	}
 
 	MONGODB_URI := os.Getenv("MONGODB_URI")
@@ -51,11 +54,12 @@ func main() {
 
 	app := fiber.New()
 
-	app.Use(cors.New(cors.Config{
-        AllowOrigins: "http://localhost:5173",
-        AllowMethods: "GET,POST,PATCH,DELETE",
-        AllowHeaders: "Origin, Content-Type, Accept",
-    }))
+	// CORS commented out for production
+	// app.Use(cors.New(cors.Config{
+    //     AllowOrigins: "http://localhost:5173",
+    //     AllowMethods: "GET,POST,PATCH,DELETE",
+    //     AllowHeaders: "Origin, Content-Type, Accept",
+    // }))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
@@ -65,6 +69,10 @@ func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "5000"
+	}
+
+	if os.Getenv("ENV") == "production" {
+		app.Static("/","./client/dist")
 	}
 
 	log.Fatal(app.Listen("0.0.0.0:" + port))
